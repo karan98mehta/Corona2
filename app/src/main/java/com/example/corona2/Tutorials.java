7647c2ed06f4659006796d253de7bd26990d8b49
Shruti Agarwal <skps.shruti@gmail.com>
2020-03-25 00:55:21 +0530

    Shruti's commit

---
+++ app/src/main/java/com/example/corona2/Tutorials.java
@@ -0,0 +1,13 @@
+package com.example.corona2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tutorials extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorials);
+    }
+}
